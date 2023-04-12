810e4a20189d03d7a483a705ad90cc593fbd9af0
Noah Cain <88724898+NCain24@users.noreply.github.com>
2023-04-12 12:48:06 -0700

    Added forecast weather functionality

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Search from './components/search/Search';
 import CurrentWeather from './components/search/current-weather/current-weather';
 import { WEATHER_API_URL } from './api';
 import { WEATHER_API_KEY } from './api';
+import Forecast from './components/search/forecast/forecast';
 
 function App() {
   const [currentWeather, setCurrentWeather] = useState(null);
@@ -19,23 +20,25 @@ function App() {
       `${WEATHER_API_URL}/forecast?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}&units=imperial`
     );
 
-    Promise.all([currentWeatherFetch, forecastFetch]).then(async (response) => {
-      const weatherResponse = await response[0].json();
-      const forecastResponse = await response[1].json();
+    Promise.all([currentWeatherFetch, forecastFetch])
+      .then(async (response) => {
+        const weatherResponse = await response[0].json();
+        const forecastResponse = await response[1].json();
 
-      setCurrentWeather({ city: searchData.label, ...weatherResponse });
-      setForecast({ city: searchData.label, ...forecastResponse });
-    } )
-    .catch((err) => console.log(err))
+        setCurrentWeather({ city: searchData.label, ...weatherResponse });
+        setForecast({ city: searchData.label, ...forecastResponse });
+      })
+      .catch((err) => console.log(err));
   };
 
-  console.log( currentWeather )
-  console.log(forecast)
+  console.log(currentWeather);
+  console.log(forecast);
 
   return (
     <div className="container">
       <Search onSearchChange={handleOnSearchChange} />
-      {currentWeather && <CurrentWeather data={currentWeather}/>}
+      {currentWeather && <CurrentWeather data={currentWeather} />}
+      {forecast && <Forecast data={forecast} />}
     </div>
   );
 }
